de3f9468b94d7681571930a47fc5c45bbff0c304
BlueEmperor <pingoleon.arcus@gmail.com>
2022-01-29 18:22:57 +0100

    za

--- Nostest/testBlue/assets/js/script.js
+++ Nostest/testBlue/assets/js/script.js
@@ -1,57 +1,11 @@
 var canvas = document.getElementById("renderCanvas");
-
-var startRenderLoop = function (engine, canvas) {
-  engine.runRenderLoop(function () {
-    if (sceneToRender && sceneToRender.activeCamera) {
-      sceneToRender.render();
-    }
-  });
-};
-
-var engine = null;
-var scene = null;
-var sceneToRender = null;
-var createDefaultEngine = function () {
-  return new BABYLON.Engine(canvas, true, {
-    preserveDrawingBuffer: true,
-    stencil: true,
-    disableWebGL2Support: false,
-  });
-};
-const createScene =  () => {
+var engine = new BABYLON.Engine(canvas, true)
+function createScene() {
     const scene = new BABYLON.Scene(engine);
-
     const camera = new BABYLON.ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 3, new BABYLON.Vector3(0, 0, 0));
     camera.attachControl(canvas, true);
-
     const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 0, 0));
-
     const box = BABYLON.MeshBuilder.CreateBox("box", {});
-
     return scene;
 }
-window.initFunction = async function () {
-  var asyncEngineCreation = async function () {
-    try {
-      return createDefaultEngine();
-    } catch (e) {
-      console.log(
-        "the available createEngine function failed. Creating the default engine instead"
-      );
-      return createDefaultEngine();
-    }
-  };
-
-  window.engine = await asyncEngineCreation();
-  if (!engine) throw "engine should not be null.";
-  startRenderLoop(engine, canvas);
-  window.scene = createScene();
-};
-initFunction().then(() => {
-  sceneToRender = scene;
-});
-
-// Resize
-window.addEventListener("resize", function () {
-  engine.resize();
-});
+createScene()
