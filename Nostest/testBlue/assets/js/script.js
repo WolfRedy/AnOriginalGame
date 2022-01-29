4847ecffa876178943e805138144583a7e36667f
BlueEmperor <pingoleon.arcus@gmail.com>
2022-01-29 19:37:49 +0100

    mouvement

--- Nostest/testBlue/assets/js/script.js
+++ Nostest/testBlue/assets/js/script.js
@@ -1,11 +1,33 @@
-var canvas = document.getElementById("renderCanvas");
-var engine = new BABYLON.Engine(canvas, true)
+const canvas = document.getElementById('renderCanvas')
+const engine = new BABYLON.Engine(canvas, true)
+let a = 0.5
 function createScene() {
     const scene = new BABYLON.Scene(engine);
-    const camera = new BABYLON.ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 3, new BABYLON.Vector3(0, 0, 0));
+    const camera = new BABYLON.ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 10, new BABYLON.Vector3(0, 0, 0));
     camera.attachControl(canvas, true);
-    const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 0, 0));
-    const box = BABYLON.MeshBuilder.CreateBox("box", {});
-    return scene;
+    const light = new BABYLON.HemisphericLight('light', new BABYLON.Vector3(0, 0, 0), scene)
+    const box = BABYLON.MeshBuilder.CreateBox('box', {}, scene)
+    const car = BABYLON.SceneLoader.ImportMesh('','./assets/babylon_object/f40obj.babylon',"", scene, {});
+    window.addEventListener("keydown", function(evt) {
+        switch(evt.keyCode) {
+            case 90: // Touche z
+                box.position = new BABYLON.Vector3(box.position._x,box.position._y,box.position._z-a)
+                break
+            case 83: // Touche s
+                box.position = new BABYLON.Vector3(box.position._x,box.position._y,box.position._z+a)
+                break
+            case 81: // Touche q
+                box.position = new BABYLON.Vector3(box.position._x+a,box.position._y,box.position._z)
+                break
+            case 68: // Touche d
+                box.position = new BABYLON.Vector3(box.position._x-a,box.position._y,box.position._z)
+                break
+        }
+    });
+    console.log(box)
+    return scene
 }
-createScene()
+const scene = createScene()
+engine.runRenderLoop(() => {
+    scene.render()
+})
