6e90b4545b78bb4cab1e54e8e31e5d5e8980a63a
Paul Viard <viardpaul83@gmail.com>
2022-01-29 14:33:18 +0100

    Sphere+box but sound problem
    J'arrive pas a faire marcher le son je comprends vraiment pas.
    Par contre manip les pos des objets tranquille.

--- Nostest/testWolf/assets/js/script.js
+++ Nostest/testWolf/assets/js/script.js
@@ -20,17 +20,17 @@ var createDefaultEngine = function () {
 };
 const createScene =  () => {
     const scene = new BABYLON.Scene(engine);
-
+    var volume = 10;
     /**** Set camera and light *****/
     const camera = new BABYLON.ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 10, new BABYLON.Vector3(0, 0, 0));
     camera.attachControl(canvas, true);
     const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(1, 1, 0));
-
+    const music = new BABYLON.Sound("retromus", "./assets/sounds/among.mp3", scene, null, { loop: true, autoplay: true, volume: volume });
+    const sphere = BABYLON.Mesh.CreateSphere("sphere", 100, 10, scene);
     const box = BABYLON.MeshBuilder.CreateBox("box", {});
-    box.position.y=1,5;
-    box.position.x=3;
-    const ground = BABYLON.MeshBuilder.CreateGround("ground", {width:10, height:10});
-
+    box.position.y =6;
+    const ground = BABYLON.MeshBuilder.CreateGround("ground", {width:1000, height:100});
+    
     return scene;
 }
 window.initFunction = async function () {
