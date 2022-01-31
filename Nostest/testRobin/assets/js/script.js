4ddd1473efe84965475da4d4db3c51e0dbdd4995
BlackJackGeary83 <robin.perdreau@gmail.com>
2022-01-31 22:57:29 +0100

    Update script.js

--- Nostest/testRobin/assets/js/script.js
+++ Nostest/testRobin/assets/js/script.js
@@ -1,118 +1,58 @@
-var canvas = document.getElementById("renderCanvas");
 
-var startRenderLoop = function (engine, canvas) {
-  engine.runRenderLoop(function () {
-    if (sceneToRender && sceneToRender.activeCamera) {
-      sceneToRender.render();
-    }
-  });
-};
-
-var engine = null;
-var scene = null;
-var sceneToRender = null;
-var createDefaultEngine = function () {
-  return new BABYLON.Engine(canvas, true, {
-    preserveDrawingBuffer: true,
-    stencil: true,
-    disableWebGL2Support: false,
-  });
-};
-var tankinoa = [];
-const createScene =  () => {
+const canvas = document.getElementById('renderCanvas')
+const engine = new BABYLON.Engine(canvas, true)
+let a = 0.5
+let objectList=[]
+function createScene() {
     const scene = new BABYLON.Scene(engine);
-    
-    /**** Set camera and light *****/
     const camera = new BABYLON.ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 10, new BABYLON.Vector3(0, 0, 0));
-    camera.attachControl(canvas, true);
-    const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(1, 1, 0));
-    const music  = new BABYLON.Sound("retromus", "./assets/sounds/among.mp3", scene); 
-        window.addEventListener("mousedown", function(evt) {
-        // left click to fire
-        if (evt.button === 0) {
-            music.play();
-          }
-        });
-        
-        window.addEventListener("keydown", function(evt) {
-          // Press space key to fire
-          if (evt.keyCode === 32) {
-            music.play();
-          }
-        });
-    
-    
+    camera.attachControl(canvas, true)
+    console.log(camera)
+    window.addEventListener("keydown", function(evt) {
+        switch(evt.keyCode) {
+            case 90: // Touche z
+                console.log('ez')
+                camera.cameraPosition = new BABYLON.Vector3(10,2,3)
+                break
+        }
+    });
+    const light = new BABYLON.HemisphericLight('light', new BABYLON.Vector3(0, 0, 0), scene)
+    const box = BABYLON.MeshBuilder.CreateBox("box", {
+        size:10
+    }, scene);
+    box.position = new BABYLON.Vector3(15,0,0)
+    const tank = BABYLON.SceneLoader.ImportMesh('','./tank.babylon',"", scene, function (newMeshes){
+        for(mesh of newMeshes) {
+            mesh.scaling = new BABYLON.Vector3(1, 1, 1)
+            mesh.position = new BABYLON.Vector3(0, 0, 0)
+            objectList.unshift(mesh)
+        }
+    }); // Tank
+    return scene
+}
 
-
-    var car = BABYLON.SceneLoader.ImportMesh("", "./", "tank.babylon", scene, function (newMeshes) {
-      // Set the target of the camera to the first imported mesh
-      for (meshes of newMeshes){
-        
-        meshes.position.x += 2
-        meshes.position.y += 2
-        meshes.position.z += 2
-
-        
-        
-        tankinoa.push(meshes);
-
-        console.log(tankinoa[0])
+const scene = createScene()
+engine.runRenderLoop(() => {
+    scene.render()
+})
 
 
 
-      }
-  });
-  
-  tankinoa[0].position.x += 1 ;
-  window.addEventListener("keydown", function(evt) {
+
+
+window.addEventListener("keydown", function(evt) {
     switch(evt.keyCode) {
         case 90: // Touche z
-        tankinoa[0].position.x += 1 ;
+            objectList[0].position.x-=a
             break
         case 83: // Touche s
-        tankinoa[0].position.x -= 1 ;
+            objectList[0].position.x+=a
             break
         case 81: // Touche q
-        tankinoa[0].position.z += 1 ;
+            objectList[0].position.z-=a
             break
         case 68: // Touche d
-        tankinoa[0].position.z -= 1 ;
+            objectList[0].position.z+=a
             break
     }
-});
-
-
-
-  
-BABYLON.SceneLoader.ImportMesh("", "./assets/", "test.babylon", scene, function (newMeshes) {
-    // Set the target of the camera to the first imported mesh
-
-});
-
-    return scene;
-}
-window.initFunction = async function () {
-  var asyncEngineCreation = async function () {
-    try {
-      return createDefaultEngine();
-    } catch (e) {
-      console.log(
-        "the available createEngine function failed. Creating the default engine instead"
-      );
-      return createDefaultEngine();
-    }
-  };
-
-  window.engine = await asyncEngineCreation();
-  if (!engine) throw "engine should not be null.";
-  startRenderLoop(engine, canvas);
-  window.scene = createScene();
-};
-initFunction().then(() => {
-  sceneToRender = scene;
-});
-
-// Resize
-window.addEventListener("resize", function () {
-  engine.resize();
 });
