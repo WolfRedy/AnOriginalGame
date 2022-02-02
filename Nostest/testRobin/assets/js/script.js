333675e5241625e1582aa862aaaa791eb6185d21
BlackJackGeary83 <robin.perdreau@gmail.com>
2022-02-02 13:40:01 +0100

    physics added

--- Nostest/testRobin/assets/js/script.js
+++ Nostest/testRobin/assets/js/script.js
@@ -1,12 +1,34 @@
 
 const canvas = document.getElementById('renderCanvas')
 const engine = new BABYLON.Engine(canvas, true)
-let a = 0.5
+let a = 0.1
 let objectList=[]
+let tankinoa
+
 function createScene() {
     const scene = new BABYLON.Scene(engine);
     const camera = new BABYLON.ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 10, new BABYLON.Vector3(0, 0, 0));
     camera.attachControl(canvas, true)
+    
+
+    //ground 
+    var ground = BABYLON.MeshBuilder.CreateBox("Ground", {width: 100, height: 1, depth: 100}, scene);
+    ground.position.y = -20.0;
+
+    var groundMat = new BABYLON.StandardMaterial("groundMat", scene);
+    groundMat.diffuseColor = new BABYLON.Color3(0.5, 0.5, 0.5);
+    groundMat.emissiveColor = new BABYLON.Color3(0.2, 0.2, 0.2);
+    groundMat.backFaceCulling = false;
+    ground.material = groundMat;
+    ground.receiveShadows = true;
+
+    var physicsRoot = new BABYLON.Mesh("", scene);
+    
+
+
+
+
+
     console.log(camera)
     window.addEventListener("keydown", function(evt) {
         switch(evt.keyCode) {
@@ -17,19 +39,57 @@ function createScene() {
         }
     });
     const light = new BABYLON.HemisphericLight('light', new BABYLON.Vector3(0, 0, 0), scene)
-    const box = BABYLON.MeshBuilder.CreateBox("box", {
-        size:10
-    }, scene);
-    box.position = new BABYLON.Vector3(15,0,0)
+    
+    
+    
     const tank = BABYLON.SceneLoader.ImportMesh('','./tank.babylon',"", scene, function (newMeshes){
         for(mesh of newMeshes) {
             
-            mesh.scaling = new BABYLON.Vector3(1, 1, 1)
-            mesh.position = new BABYLON.Vector3(0, 0, 0)
+ 
             console.log(mesh)
             objectList.unshift(mesh)
+            physicsRoot.addChild(mesh)
         }
+
+
+    
+
+
+
+
     }); // Tank
+
+
+
+    //Physics
+    scene.enablePhysics(null, new BABYLON.CannonJSPlugin());
+    
+    ground.physicsImpostor = new BABYLON.PhysicsImpostor(ground, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0, friction: 0.0, restitution: 0.7 }, scene);
+
+    const box = BABYLON.MeshBuilder.CreateBox("box", {size:10}, scene);
+    box.isVisible = false;
+
+    
+    physicsRoot.addChild(box)
+    box.physicsImpostor = new BABYLON.PhysicsImpostor(box, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0, friction: 0.0, restitution: 0 }, scene);
+    physicsRoot.physicsImpostor = new BABYLON.PhysicsImpostor(physicsRoot, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 5, friction: 0.0, restitution: 0.7 }, scene);
+    
+
+    var impulseDirection = new BABYLON.Vector3(0, 1, 0);
+    var impulseMagnitude = 5;
+    var contactLocalRefPoint = BABYLON.Vector3.Zero();
+    var Pulse = function(meshtopulse) {
+        meshtopulse.physicsImpostor.applyImpulse(impulseDirection.scale(impulseMagnitude), box.getAbsolutePosition().add(contactLocalRefPoint));
+    }
+
+    window.addEventListener("keydown", function(evt) {
+        switch(evt.keyCode) {
+            case 32:
+            Pulse(physicsRoot)
+            console.log(physicsRoot)
+    }});
+
+
     return scene
 }
 
@@ -56,5 +116,6 @@ window.addEventListener("keydown", function(evt) {
         case 68: // Touche d
             objectList[0].position.z+=a
             break
+
     }
 });
