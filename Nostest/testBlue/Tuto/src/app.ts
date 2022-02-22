cacc6d8078c51c4c12044e9ab85fad048eea7876
BlueEmperor <pingoleon.arcus@gmail.com>
2022-02-22 21:24:49 +0100

    héhé

--- Nostest/testBlue/Tuto/src/app.ts
+++ Nostest/testBlue/Tuto/src/app.ts
@@ -4,7 +4,7 @@ import "@babylonjs/loaders/glTF";
 import { Engine, Scene, ArcRotateCamera, Vector3, HemisphericLight, Mesh, MeshBuilder, Color4, FreeCamera, SceneLoader } from "@babylonjs/core";
 import {AdvancedDynamicTexture, Button, Control} from "@babylonjs/gui";
 
-enum GameState { START = 0, GAME = 1, PAUSE = 2, ANIMATION = 3 }
+enum GameState { STARTMENU = 0, GAME = 1, PAUSE = 2, ANIMATION = 3 }
 
 class App {
     private _scene: Scene;
@@ -27,12 +27,15 @@ class App {
         // Racourcis de Debug
         window.addEventListener("keydown", (ev) => {
             // Shift+Ctrl+Alt+I
-            if (ev.shiftKey && ev.ctrlKey && ev.altKey && ev.keyCode === 73) {
+            if (ev.shiftKey && ev.ctrlKey && ev.altKey && ev.keyCode == 73) {
                 if (this._scene.debugLayer.isVisible()) {
                     this._scene.debugLayer.hide();
                 } else {
                     this._scene.debugLayer.show();
                 }
+            }
+            if (ev.keyCode == 90) {
+                this._scene.detachControl();
             }
         });
 
@@ -42,12 +45,12 @@ class App {
 
     // Fonction Main
     private async _main(): Promise<void> {
-        await this._start();
+        await this._startMenu();
 
         // Render Loop
         this._engine.runRenderLoop(() => {
             switch (this._state) {
-                case GameState.START:
+                case GameState.STARTMENU:
                     this._scene.render();
                     break;
                 case GameState.GAME:
@@ -87,8 +90,21 @@ class App {
     }
 
     // Menu de base
-    private async _start() {
+    private async _startMenu() {
+        let scene = this._scene
         this._engine.displayLoadingUI();
+        
+        // Camera
+        let camera: ArcRotateCamera = new ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2, Vector3.Zero(), scene);
+        camera.attachControl(this._canvas, true);
+        
+        // Objets
+        var sphere: Mesh = MeshBuilder.CreateSphere("sphere", { diameter: 1 }, this._scene);
+        var light1: HemisphericLight = new HemisphericLight("light1", new Vector3(1, 1, 0), this._scene);
+
+        // Attente de fin de render
+        await scene.whenReadyAsync();
+        this._engine.hideLoadingUI();
     }
 }
 new App();
